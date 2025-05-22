7c79a19de86fd737af467ad2e60a6902f16828d1
Anuj3558 <anujloharkar3557@gmail.com>
2025-05-22 17:02:41 +0530

    updat

--- src/assets/Audios/audio.js
+++ src/assets/Audios/audio.js
@@ -2,7 +2,6 @@ import as from "./AS Roofing_sample_recording.wav"
 import recruit from "./Recruitment_call_female_new_2.mp3";
 import herky from "./herkey_event_confirmation.wav";
 import Flyfin from "./Flyfin_tax_filing.wav"
-import vid from "./bg_0048.mp4";
 export{
     recruit,
     herky,
